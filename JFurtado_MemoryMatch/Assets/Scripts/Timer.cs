42cdcef981ee8bc658aa385d70f2433d865d1375
Justin Furtado <justin.l.furtado@gmail.com>
2017-02-14 16:59:52 -0700

    Add basic timer functionality

---
+++ JFurtado_MemoryMatch/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public float TimeRemaining = 30.0f;
+    public GameManager GM;
+    public Text TimeRemainingText;
+    private bool isOver;
+    private const int NUM_CHARS = 6;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        TimeRemaining -= Time.deltaTime;
+        isOver = TimeRemaining <= 0.0f;
+        SetText();
+
+        if (isOver)
+        {
+            GM.GoToGameOverScene();
+        }
+	}
+
+    private void SetText()
+    {
+        string timeString = TimeRemaining.ToString();
+        TimeRemainingText.text = "Text: " + timeString.Substring(0, Mathf.Min(NUM_CHARS, timeString.Length));
+    }
+}
