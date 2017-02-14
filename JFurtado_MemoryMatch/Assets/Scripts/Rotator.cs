f50df6c8933c9ebeb0211796b92d7a14559ffc1d
Justin Furtado <justin.l.furtado@gmail.com>
2017-02-14 16:21:25 -0700

    Add fancy background to title screen

---
+++ JFurtado_MemoryMatch/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public float RotationSpeed = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.forward, RotationSpeed * Time.deltaTime);
+	}
+}
