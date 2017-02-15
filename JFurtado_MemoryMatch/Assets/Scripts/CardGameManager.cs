231c07afafce9e63a821069abced85bc93d879b7
Justin Furtado <justin.l.furtado@gmail.com>
2017-02-14 20:58:42 -0700

    Add basic Matching functionality

---
+++ JFurtado_MemoryMatch/Assets/Scripts/CardGameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardGameManager : MonoBehaviour {
+
+    public bool IsCardRotating = false;
+    private CardManager currentFlippedCard;
+    public Sprite[] cardImages;
+
+    // Use this for initialization
+    void Start () {
+        currentFlippedCard = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCardClicked(CardManager cm)
+    {
+        // if a card is currently flipped
+        if (currentFlippedCard != null)
+        {
+            if (currentFlippedCard.ImageIndex == cm.ImageIndex)
+            {
+                TellCardToDie(cm);
+                TellCardToDie(currentFlippedCard);
+                currentFlippedCard = null;
+                IsCardRotating = false;
+            }
+        }
+        else
+        {
+            currentFlippedCard = cm;
+            IsCardRotating = true;
+            cm.StartFlip();
+        }
+    }
+
+    private void TellCardToDie(CardManager cm)
+    {
+        cm.OnMatched();
+    }
+
+    public void OnCardFlipEnd()
+    {
+        IsCardRotating = false;
+    }
+}
--- JFurtado_MemoryMatch/Assets/Scripts/CardManager.cs
+++ JFurtado_MemoryMatch/Assets/Scripts/CardManager.cs
@@ -5,36 +5,64 @@ using UnityEngine;
 public class CardManager : MonoBehaviour {
 
     public float BaseRotationSpeed;
-
+    public int ImageIndex;
     private float rotation = 0.0f;
     private float rotationSpeed = 0.0f;
-    private const float rotationThreshold = 3.141592654f;
+    private float rotationThreshold = Mathf.PI;
     private bool isRotating = false; // TODO: MANAGER!?!?!?
+    private CardGameManager cgm;
+    private Vector3 currentAxis;
+    private bool shouldDie;
 
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+        cgm = transform.parent.gameObject.GetComponent<CardGameManager>();
+        shouldDie = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
         float rotateAmount = rotationSpeed * Time.deltaTime;
-        transform.RotateAround(Vector3.up, rotateAmount);
+        transform.RotateAround(currentAxis, rotateAmount);
         rotation += rotateAmount;
 
         if (rotation > rotationThreshold)
         {
             stopRotate();
+
+            if (shouldDie)
+            {
+                this.GetComponent<Renderer>().enabled = false;
+                this.enabled = false;
+            }
         }
 	}
 
     void OnMouseDown()
     {
-        startRotate();
+        if (!isRotating && !cgm.IsCardRotating)
+        {
+            cgm.OnCardClicked(this);
+        }
     }
 
-    private void startRotate()
+    public void OnMatched()
     {
-        if (!isRotating)
-        {
-            rotationSpeed = BaseRotationSpeed;
-            isRotating = true;
-        }
+        shouldDie = true;
+        startRotate(Vector3.forward, 3.0f*Mathf.PI);
+    }
+
+    public void StartFlip()
+    {
+        startRotate(Vector3.up, Mathf.PI);
+    }
+
+    private void startRotate(Vector3 axis, float threshold)
+    {
+        rotationSpeed = BaseRotationSpeed;
+        isRotating = true;
+        currentAxis = axis;
+        rotationThreshold = threshold;
     }
 
     private void stopRotate()
@@ -42,5 +70,6 @@ public class CardManager : MonoBehaviour {
         rotationSpeed = 0.0f;
         rotation = 0.0f;
         isRotating = false;
+        cgm.OnCardFlipEnd();
     }
 }
