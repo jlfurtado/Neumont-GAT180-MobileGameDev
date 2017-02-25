940c903cb7e3ce81f0bfb0076c7b3c336b46c84a
Justin Furtado <justin.l.furtado@gmail.com>
2017-02-24 17:19:08 -0700

    Add some flow

---
+++ FinalProject/Assets/Scripts/Strings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+static class Strings
+{
+    public static string LEVEL_SELECT_SCENE_NAME = "LevelSelectScene";
+}
+
