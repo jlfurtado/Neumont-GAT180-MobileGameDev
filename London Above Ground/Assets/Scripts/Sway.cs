83a2dc7d43a169b4707905cb3e706129a0f5d2a7
Justin Furtado <justin.l.furtado@gmail.com>
2017-02-27 16:31:27 -0700

    Update Lamp thingy

---
+++ London Above Ground/Assets/Scripts/Sway.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sway : MonoBehaviour {
+    public float maxRotation;
+    public float minRotation;
+    public float rotationSpeed;
+    private float rotation;
+    private float sign = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+        rotation += sign * rotationSpeed * Time.deltaTime;
+        if (rotation > maxRotation || rotation < minRotation) { sign *= -1.0f; rotation = Mathf.Clamp(rotation, minRotation, maxRotation); }
+        transform.rotation = Quaternion.Euler(new Vector3(rotation, 0.0f, 0.0f));
+	}
+}
